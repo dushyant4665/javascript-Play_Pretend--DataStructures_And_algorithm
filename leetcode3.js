ad46e10961bcdd794c4dccd41e7f16a6eef8144f
Dushyant Khandelwal <dushyantkhandelwal4665@gmail.com>
2024-09-10 22:22:12 +0530

    add

---
+++ leetcode3.js
@@ -0,0 +1,27 @@
+// Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+
+function expect (val){
+    return {
+        toBe:function(valComp){
+        if(val===valComp){
+            return true;
+        }
+        else{
+         throw new Error ('Not Equal')
+        }
+    },
+    notToBe:function(valComp){
+    if (val!==valComp){
+        return true; 
+    }
+    else{
+     throw new Error ('Equal')
+    }
+}
+};
+}
+try{
+    console.log(expect(5).toBe(5))
+}catch{
+    console.error(error.message)
+}
