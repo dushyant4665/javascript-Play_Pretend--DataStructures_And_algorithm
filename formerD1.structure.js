34f90b6ccdea79b589f5ec1db015f606917e7d80
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-10-14 00:08:53 +0530

    Update formerD1.structure.js

--- formerD1.structure.js
+++ formerD1.structure.js
@@ -1,6 +1,7 @@
 
 
-// create an array with 8 student names, after that crete a function which takes 2 parameters(allStudents & studentName), iterate over all students and find that specific user 'studentName'
+// create an array with 8 student names, after that crete a function which takes 2 parameters(allStudents & studentName),
+ // iterate over all students and find that specific user 'studentName'
 
 const allStudent=['vedant','siddharth','lokesh','dushyant','aditya','nehar','vijay','vaibhav']
 
@@ -21,71 +22,3 @@ var findStudent=(allStudent,studentName)=>{
 
 findStudent(allStudent,'vaibhav')
 
-// var findStudent=(allStudent,studentName)=>{
-//     // for(let i=0; i<allStudent.length; i++ ){
-
-//     // }
-//     let i=0; 
-//     i<allStudent.length; 
-//     i++;
-
-//     if(allStudent[i]===studentName){
-//         console.log(`found ${studentName}`)
-//     }
-//     else{
-//         console.log('not found')
-//     }
-// }
-
-
-// findStudent(allStudent,'siddharth')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const allStudents=['aman','bharat','chetan','dushyant','eman','farukl','golu','hitesh']
-
-
-// const findStudent=(allStudents,studentName)=>{
-//     for(let i =0; i<allStudents.length; i++){
-//         if(allStudents[i]===studentName){
-//             console.log(`found ${studentName} at ${[i]}`)
-//         }
-//         // else{
-//         //     console.log('not found')
-//         // }
-//     }
-// }
-
-// findStudent(allStudents,'eman')
-
-
-
-
-
-// const allStudent=['hitesh','vikash','chavan','swara','manish','dipesh','rancho','ayush']
-
-// var findStudent=(allStudent,studentName)=>{
-         
-//        for(let i=0;i<allStudent.length;i++){
-//         if(allStudent[i]===studentName){
-//             console.log(`found ${studentName} at ${i}`)
-//         }
-//         else{
-//             // console.log('not found')
-//         }
-//     }
-// }
-
-// findStudent(allStudent,'rancho')
