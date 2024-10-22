9dc9d9e3b21670aeb076343979d67f7b51fcca73
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-10-22 23:31:05 +0530

    Update leetcode6.js

--- leetcode6.js
+++ leetcode6.js
@@ -8,12 +8,12 @@
 
 
 function map(arr, fn) {
-    const returnedArray = []; // Nayi array jo result store karegi
+    const returnedArray = [];            //nayi array jo result store karegi
     
-    // Input array ke har element pe loop karo
+                                        //input array ke har element pe loop karo
     for (let i = 0; i < arr.length; i++) {
-      returnedArray.push(fn(arr[i], i)); // Function fn apply karke result store karo
+      returnedArray.push(fn(arr[i], i)); //function fn apply karke result store karo
     }
     
-    return returnedArray; // Transformed array return karo
+    return returnedArray; //transformed array return karo
   }
